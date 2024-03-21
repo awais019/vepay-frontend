bfe833a33daed6a4993802400871e31496765fb5
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-03-21 15:00:09 +0500

    Added acknowledgment section.

---
+++ components/Acknowledgment.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section
+    class="mt-6 py-6 sm:py-8 md:py-10 lg:py-12 xl:py-16 2xl:py-20 max-w-3xl mx-auto text-center"
+  >
+    <h3
+      class="text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-semibold"
+    >
+      Thank you for your Interest in VePay.
+    </h3>
+    <p>
+      We would love to hear from you and discuss how we can help bring your
+      digital ideas to life. Here are the different ways you can get in touch
+      with us.
+    </p>
+    <button
+      class="mt-10 py-1 px-2 md:px-4 md:py-3 lg:px-6 xl:px-8 xl:py-4 bg-green-50 md:text-lg font-medium text-grey-10 rounded-lg"
+    >
+      Download App
+    </button>
+  </section>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
   <WhyChooseUs />
   <Reviews />
   <FAQs />
+  <Acknowledgment />
 </template>
 
 <style scoped></style>
