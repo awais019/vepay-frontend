1ad31e557d0da1e12ccbb933a7084b1267c42bf6
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-03-21 10:33:27 +0500

    Added for card.

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -18,6 +18,40 @@
       Empowering Consumers, Resellers, and Manufacturers with Blockchain-Enabled
       Verification and Seamless Transactions
     </p>
+    <div class="my-7 flex flex-col justify-center items-center gap-7 md:hidden">
+      <div
+        class="py-2 px-8 rounded-lg border border-grey-15 bg-grey-12 bg-opacity-20 backdrop-blur-sm"
+      >
+        <div
+          class="py-3 px-10 border border-grey-15 rounded-xl text-xl bg-grey-12 bg-opacity-20 backdrop-blur-sm"
+        >
+          For
+        </div>
+      </div>
+      <div class="py-3 px-4 rounded-lg bg-grey-1 text-sm">Consumers</div>
+      <div class="py-3 px-4 rounded-lg bg-grey-1 text-sm">Resellers</div>
+      <div class="py-3 px-4 rounded-lg bg-grey-1 text-sm">Manufacturers</div>
+      <div class="py-3 px-4 rounded-lg bg-grey-1 text-sm">Social Good</div>
+    </div>
+    <div
+      class="py-4 px-8 max-w-fit mx-auto rounded-lg border border-grey-15 items-center gap-2 bg-grey-12 bg-opacity-20 backdrop-blur-sm hidden md:flex"
+    >
+      <div class="text-2xl text-grey-60">For</div>
+      <div class="py-3 px-4 rounded-lg text-xl lg:text-2xl bg-grey-15">
+        Consumers
+      </div>
+      <span class="text-grey-60 self-end">,</span>
+      <div class="py-3 px-4 rounded-lg text-xl lg:text-2xl bg-grey-15">
+        Resellers
+      </div>
+      <div class="py-3 px-4 rounded-lg text-xl lg:text-2xl bg-grey-15">
+        Manufacturers
+      </div>
+      <span class="text-grey-60 text-2xl"> and </span>
+      <div class="py-3 px-4 rounded-lg text-xl lg:text-2xl bg-grey-15">
+        Social Good
+      </div>
+    </div>
   </section>
 </template>
 
