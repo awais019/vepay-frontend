48444a3bee79d5ff605a4d6ed886e1b293458a3d
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-03-21 10:55:03 +0500

    Added bg abstract pattern in hero section.

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -18,65 +18,69 @@
       Empowering Consumers, Resellers, and Manufacturers with Blockchain-Enabled
       Verification and Seamless Transactions
     </p>
-    <div class="my-7 flex flex-col justify-center items-center gap-7 md:hidden">
+    <div class="bg-hero-pattern bg-no-repeat bg-cover bg-center">
       <div
-        class="py-2 px-8 rounded-lg border border-grey-15 bg-grey-12 bg-opacity-20 backdrop-blur-sm"
+        class="my-7 flex flex-col justify-center items-center gap-7 md:hidden"
       >
         <div
-          class="py-3 px-10 border border-grey-15 rounded-xl text-xl bg-grey-12 bg-opacity-20 backdrop-blur-sm"
+          class="py-2 px-8 rounded-lg border border-grey-15 bg-grey-12 bg-opacity-20 backdrop-blur-sm"
         >
-          For
+          <div
+            class="py-3 px-10 border border-grey-15 rounded-xl text-xl bg-grey-12 bg-opacity-20 backdrop-blur-sm"
+          >
+            For
+          </div>
+        </div>
+        <div class="py-3 px-4 rounded-lg bg-grey-1 text-sm">Consumers</div>
+        <div class="py-3 px-4 rounded-lg bg-grey-1 text-sm">Resellers</div>
+        <div class="py-3 px-4 rounded-lg bg-grey-1 text-sm">Manufacturers</div>
+        <div class="py-3 px-4 rounded-lg bg-grey-1 text-sm">Social Good</div>
+      </div>
+      <div
+        class="py-4 my-7 px-8 max-w-fit mx-auto rounded-lg border border-grey-15 items-center gap-2 bg-grey-12 bg-opacity-20 backdrop-blur-sm hidden md:flex"
+      >
+        <div class="text-2xl text-grey-60">For</div>
+        <div class="py-3 px-4 rounded-lg text-xl lg:text-2xl bg-grey-15">
+          Consumers
+        </div>
+        <span class="text-grey-60 self-end">,</span>
+        <div class="py-3 px-4 rounded-lg text-xl lg:text-2xl bg-grey-15">
+          Resellers
+        </div>
+        <div class="py-3 px-4 rounded-lg text-xl lg:text-2xl bg-grey-15">
+          Manufacturers
+        </div>
+        <span class="text-grey-60 text-2xl"> and </span>
+        <div class="py-3 px-4 rounded-lg text-xl lg:text-2xl bg-grey-15">
+          Social Good
         </div>
       </div>
-      <div class="py-3 px-4 rounded-lg bg-grey-1 text-sm">Consumers</div>
-      <div class="py-3 px-4 rounded-lg bg-grey-1 text-sm">Resellers</div>
-      <div class="py-3 px-4 rounded-lg bg-grey-1 text-sm">Manufacturers</div>
-      <div class="py-3 px-4 rounded-lg bg-grey-1 text-sm">Social Good</div>
-    </div>
-    <div
-      class="py-4 my-7 px-8 max-w-fit mx-auto rounded-lg border border-grey-15 items-center gap-2 bg-grey-12 bg-opacity-20 backdrop-blur-sm hidden md:flex"
-    >
-      <div class="text-2xl text-grey-60">For</div>
-      <div class="py-3 px-4 rounded-lg text-xl lg:text-2xl bg-grey-15">
-        Consumers
+      <div class="flex items-center justify-center gap-4">
+        <NuxtLink
+          to="/about"
+          class="border border-grey-20 bg-grey-12 bg-opacity-20 backdrop-blur-sm py-3 px-5 md:py-5 md:px-7 rounded-lg text-sm md:text-lg font-medium"
+          >About</NuxtLink
+        >
+        <NuxtLink
+          to="/contact"
+          class="py-3 px-5 bg-green-50 rounded-lg text-grey-15 text-sm font-medium md:py-5 md:px-7 md:text-lg"
+          >Contact Us</NuxtLink
+        >
       </div>
-      <span class="text-grey-60 self-end">,</span>
-      <div class="py-3 px-4 rounded-lg text-xl lg:text-2xl bg-grey-15">
-        Resellers
-      </div>
-      <div class="py-3 px-4 rounded-lg text-xl lg:text-2xl bg-grey-15">
-        Manufacturers
-      </div>
-      <span class="text-grey-60 text-2xl"> and </span>
-      <div class="py-3 px-4 rounded-lg text-xl lg:text-2xl bg-grey-15">
-        Social Good
-      </div>
-    </div>
-    <div class="flex items-center justify-center gap-4">
-      <NuxtLink
-        to="/about"
-        class="border border-grey-20 bg-grey-12 bg-opacity-20 backdrop-blur-sm py-3 px-5 md:py-5 md:px-7 rounded-lg text-sm md:text-lg font-medium"
-        >About</NuxtLink
-      >
-      <NuxtLink
-        to="/contact"
-        class="py-3 px-5 bg-green-50 rounded-lg text-grey-15 text-sm font-medium md:py-5 md:px-7 md:text-lg"
-        >Contact Us</NuxtLink
-      >
-    </div>
-    <div class="my-10">
-      <p
-        class="py-3 px-6 rounded-full border border-grey-15 bg-grey-10 text-sm md:text-lg font-medium max-w-fit mx-auto text-center"
-      >
-        Trusted by more than 250+ Customers
-      </p>
-      <div class="flex gap-5 justify-center items-center flex-wrap">
-        <div class="py-5 px-2 md:py-10 md:px-5"><Zapier /></div>
-        <div class="py-5 px-2 md:py-10 md:px-5"><Spotify /></div>
-        <div class="py-5 px-2 md:py-10 md:px-5"><Zoom /></div>
-        <div class="py-5 px-2 md:py-10 md:px-5"><Slack /></div>
-        <div class="py-5 px-2 md:py-10 md:px-5"><Amazon /></div>
-        <div class="py-5 px-2 md:py-10 md:px-5"><Adobe /></div>
+      <div class="my-10">
+        <p
+          class="py-3 px-6 rounded-full border border-grey-15 bg-grey-10 text-sm md:text-lg font-medium max-w-fit mx-auto text-center"
+        >
+          Trusted by more than 250+ Customers
+        </p>
+        <div class="flex gap-5 justify-center items-center flex-wrap">
+          <div class="py-5 px-2 md:py-10 md:px-5"><Zapier /></div>
+          <div class="py-5 px-2 md:py-10 md:px-5"><Spotify /></div>
+          <div class="py-5 px-2 md:py-10 md:px-5"><Zoom /></div>
+          <div class="py-5 px-2 md:py-10 md:px-5"><Slack /></div>
+          <div class="py-5 px-2 md:py-10 md:px-5"><Amazon /></div>
+          <div class="py-5 px-2 md:py-10 md:px-5"><Adobe /></div>
+        </div>
       </div>
     </div>
   </section>
